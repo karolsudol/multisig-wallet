364cdf28525eeff46543fc572b1d8a90a147243a
elcomendante <karol@finsane.com>
2022-12-01 18:09:31 +0000

    tests redone

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,22 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  typechain: {
+    outDir: "src/typechain-types",
+    target: "ethers-v5",
+    alwaysGenerateOverloads: false, // should overloads with full signatures
+    // like deposit(uint256) be generated always,
+    // even if there are no overloads?
+    externalArtifacts: ["externalArtifacts/*.json"], // optional array of glob
+    // patterns with external artifacts to process (for example external libs
+    // from node_modules)
+    dontOverrideCompile: false, // defaults to false
+  },
 };
 
 export default config;
--- test/multisig-test.ts
+++ test/multisig-test.ts
@@ -1,49 +1,46 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { formatEther, parseEther, parseUnits } from "ethers/lib/utils";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { MultiSigWallet } from "../src/typechain-types/MultiSigWallet";
 
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Signer } from "ethers";
-
-// const WeiToEther = formatEther(weiValue);
-// const EtherToWei = parseUnits("0.11","ether")
 
 describe("MultiSigWallet", () => {
-  async function deploy() {
+  let multiSigWallet: MultiSigWallet;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+  let addr3: SignerWithAddress;
+  let addr4: SignerWithAddress;
+
+  beforeEach(async function () {
+    [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
     const MultiSigWallet = await ethers.getContractFactory("MultiSigWallet");
-
-    let signers: Array<Signer>;
-    signers = await ethers.getSigners();
     const quorum = 2;
-    const owners = [
-      await signers[0].getAddress(),
-      await signers[1].getAddress(),
-      await signers[2].getAddress(),
-    ];
-    const multiSigWallet = await MultiSigWallet.deploy(owners, quorum);
+    // const web3 = MultiSigWallet.web3;
 
-    return {
-      multiSigWallet,
-      signers,
-    };
-  }
+    multiSigWallet = await MultiSigWallet.deploy(
+      [addr1.address, addr2.address, addr3.address],
+      quorum
+    );
+    await multiSigWallet.deployed();
+  });
 
-  describe("receive", async function () {
+  describe("deposit and receive", async function () {
     it("should receive correct ammount and emit deposit event", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-
       expect(
         formatEther(await ethers.provider.getBalance(multiSigWallet.address))
       ).to.equal(formatEther("0"));
 
-      const tx = signers[0].sendTransaction({
+      const tx = addr1.sendTransaction({
         to: multiSigWallet.address,
         value: parseEther("100"),
       });
       await expect(tx)
         .to.emit(multiSigWallet, "Deposit")
         .withArgs(
-          await signers[0].getAddress(),
+          await addr1.getAddress(),
           parseEther("100"),
           parseEther("100")
         );
@@ -55,26 +52,32 @@ describe("MultiSigWallet", () => {
   });
 
   describe("submit transaction", async function () {
-    it("should submit transaction correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-      const txCount0 = await multiSigWallet.getTransactionCount();
-
-      const to = await signers[1].getAddress();
+    it("should revert submiting transaction", async () => {
+      const to = addr1.address;
       const value = parseEther("10");
       const data = "0x00";
 
       await expect(
-        multiSigWallet.connect(signers[3]).submitTransaction(to, value, data)
+        multiSigWallet.connect(addr4).submitTransaction(to, value, data)
       ).to.be.revertedWith("not owner");
+    });
 
-      // Submit a proposal to send 10 ether to signer1's address
+    it("should submit transaction", async () => {
+      const txCount0 = await multiSigWallet.getTransactionCount();
+
+      const to = addr4.address;
+      const value = parseEther("10");
+      const data = "0x00";
+
+      // Submit a proposal to send 10 ether to acc4
       const tx = await multiSigWallet
-        .connect(signers[0])
+        .connect(addr1)
         .submitTransaction(to, value, data);
       await tx.wait();
 
-      const txCount1 = await multiSigWallet.getTransactionCount();
-      expect(txCount1).to.equal(txCount0.add(1));
+      expect(await multiSigWallet.getTransactionCount()).to.equal(
+        txCount0.add(1)
+      );
 
       const transaction = await multiSigWallet.getTransaction(0);
       expect(transaction.to).to.equal(to);
@@ -85,21 +88,21 @@ describe("MultiSigWallet", () => {
 
       await expect(tx)
         .to.emit(multiSigWallet, "SubmitTransaction")
-        .withArgs(await signers[0].getAddress(), 0, to, value, data);
+        .withArgs(addr1.address, 0, to, value, data);
     });
   });
 
   describe("update owners ", async function () {
-    it.only("should revoke adding owner correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-
+    it("should revoke adding owner", async () => {
       await expect(
-        multiSigWallet.connect(signers[3]).addOwner(signers[3].getAddress())
+        multiSigWallet.connect(addr4).addOwner(addr4.getAddress())
       ).to.be.revertedWith("only wallet");
 
-      await expect(
-        multiSigWallet.addOwner(signers[4].getAddress())
-      ).to.be.revertedWith("only wallet");
+      // await expect(
+      //   multiSigWallet
+      //     .connect(multiSigWallet.address)
+      //     .addOwner(signers[4].getAddress())
+      // ).to.be.revertedWith("only wallet");
 
       // await expect(
       //   multiSigWallet.addOwner(signers[3].getAddress())
@@ -109,9 +112,7 @@ describe("MultiSigWallet", () => {
       //   await multiSigWallet.addOwner(signers[0].getAddress())
       // ).to.be.revertedWith("only wallet");
     });
-    it.only("should add owner correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-
+    it("should add owner", async () => {
       expect((await multiSigWallet.getOwners()).length).equal(3);
 
       // await expect(multiSigWallet.addOwner(signers[3].getAddress()))
@@ -121,217 +122,128 @@ describe("MultiSigWallet", () => {
       // expect((await multiSigWallet.getOwners()).length).equal(4);
     });
 
-    it("should remove owner correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-    });
+    it("should remove owner", async () => {});
   });
 
   describe("revoke tx confirmations", async function () {
-    it("should revert for non-owner correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
+    it("should revert for non-owner", async () => {
       await expect(
-        multiSigWallet.connect(signers[3]).revokeConfirmation(0)
+        multiSigWallet.connect(addr4).revokeConfirmation(0)
       ).to.be.revertedWith("not owner");
     });
 
-    it("should revert if tx doesn't exist correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
+    it("should revert if tx doesn't exist", async () => {
       await expect(
-        multiSigWallet.connect(signers[0]).revokeConfirmation(1)
+        multiSigWallet.connect(addr1).revokeConfirmation(1)
       ).to.be.revertedWith("tx does not exist");
     });
 
-    it("should revert if tx already executed correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
+    it("should revert if not enough confirmation to execute", async () => {
+      const to = addr4.address;
+      const value = parseEther("10");
+      const data = "0x00";
 
-      const tx = await multiSigWallet
-        .connect(signers[0])
-        .submitTransaction(
-          await signers[3].getAddress(),
-          ethers.utils.parseEther("10"),
-          "0x00"
-        );
-      await tx.wait();
-      signers[0].sendTransaction({
+      await multiSigWallet.connect(addr1).submitTransaction(to, value, data);
+
+      await addr1.sendTransaction({
         to: multiSigWallet.address,
-        value: ethers.utils.parseEther("10"),
+        value: value,
       });
 
-      await multiSigWallet.connect(signers[0]).confirmTransaction(0);
+      await multiSigWallet.connect(addr1).confirmTransaction(0);
 
-      await signers[1].sendTransaction({
-        to: multiSigWallet.address,
-        value: ethers.utils.parseEther("10"),
-      });
-      await multiSigWallet.connect(signers[1]).confirmTransaction(0);
-      await multiSigWallet.executeTransaction(0);
       await expect(
-        multiSigWallet.connect(signers[1]).revokeConfirmation(0)
+        multiSigWallet.connect(addr1).executeTransaction(0)
+      ).to.be.revertedWith("not enough confirmation to execute");
+    });
+
+    it("should revert if tx already executed", async () => {
+      const to = addr4.address;
+      const value = parseEther("10");
+      const data = "0x00";
+
+      await multiSigWallet.connect(addr1).submitTransaction(to, value, data);
+
+      await addr1.sendTransaction({
+        to: multiSigWallet.address,
+        value: value,
+      });
+
+      await multiSigWallet.connect(addr1).confirmTransaction(0);
+      await multiSigWallet.connect(addr2).confirmTransaction(0);
+      await multiSigWallet.connect(addr1).executeTransaction(0);
+
+      await expect(
+        multiSigWallet.connect(addr3).executeTransaction(0)
       ).to.be.revertedWith("tx already executed");
     });
 
-    it("should revert if not confirmed correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-      const tx = await multiSigWallet
-        .connect(signers[0])
-        .submitTransaction(
-          await signers[3].getAddress(),
-          ethers.utils.parseEther("10"),
-          "0x00"
-        );
-      await tx.wait();
-      signers[0].sendTransaction({
+    it("should revoke for owner that confirmed prior", async () => {
+      const to = addr2.address;
+      const value = parseEther("10");
+      const data = "0x00";
+
+      await multiSigWallet.connect(addr1).submitTransaction(to, value, data);
+
+      await addr1.sendTransaction({
         to: multiSigWallet.address,
-        value: ethers.utils.parseEther("10"),
+        value: value,
       });
 
-      await multiSigWallet.connect(signers[0]).confirmTransaction(0);
+      await multiSigWallet.connect(addr1).confirmTransaction(0);
 
-      await expect(
-        multiSigWallet.connect(signers[1]).revokeConfirmation(0)
-      ).to.be.revertedWith("tx not confirmed");
-    });
-
-    it("should revoke for owner that confirmed prior, correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-
-      await multiSigWallet
-        .connect(signers[0])
-        .submitTransaction(
-          await signers[3].getAddress(),
-          ethers.utils.parseEther("10"),
-          "0x00"
-        );
-
-      await signers[0].sendTransaction({
-        to: multiSigWallet.address,
-        value: ethers.utils.parseEther("10"),
-      });
-
-      await multiSigWallet.connect(signers[0]).confirmTransaction(0);
-
-      const tx = await multiSigWallet.connect(signers[0]).revokeConfirmation(0);
+      const tx = await multiSigWallet.connect(addr1).revokeConfirmation(0);
       const transaction = await multiSigWallet.getTransaction(0);
       expect(transaction.numConfirmations).to.equal(0);
-      expect(await multiSigWallet.isConfirmed(0, await signers[0].getAddress()))
-        .to.be.false;
+      expect(await multiSigWallet.isConfirmed(0, addr1.address)).to.be.false;
       await expect(tx)
         .to.emit(multiSigWallet, "RevokeConfirmation")
-        .withArgs(await signers[0].getAddress(), 0);
+        .withArgs(addr1.address, 0);
     });
   });
 
   describe("execute transactions", async function () {
-    it("should revert for non owner correctly ", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-      await multiSigWallet
-        .connect(signers[0])
-        .submitTransaction(
-          await signers[3].getAddress(),
-          ethers.utils.parseEther("10"),
-          "0x00"
-        );
-      await signers[0].sendTransaction({
+    it("should revert for non owner ", async () => {
+      const to = addr2.address;
+      const value = parseEther("10");
+      const data = "0x00";
+
+      await multiSigWallet.connect(addr1).submitTransaction(to, value, data);
+      await addr1.sendTransaction({
         to: multiSigWallet.address,
-        value: ethers.utils.parseEther("10"),
+        value: value,
       });
 
       // quorum achived
-      for (let i of [0, 1]) {
-        const tx = await multiSigWallet
-          .connect(signers[i])
-          .confirmTransaction(0);
-        await tx.wait();
-      }
+      await multiSigWallet.connect(addr1).confirmTransaction(0);
+      await multiSigWallet.connect(addr2).confirmTransaction(0);
+
       await expect(
-        multiSigWallet.connect(signers[3]).executeTransaction(0)
+        multiSigWallet.connect(addr4).executeTransaction(0)
       ).to.be.revertedWith("not owner");
     });
 
-    it("should revert if transaction doesn't exist correctly ", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-      await multiSigWallet
-        .connect(signers[0])
-        .submitTransaction(
-          await signers[3].getAddress(),
-          ethers.utils.parseEther("10"),
-          "0x00"
-        );
-      await signers[0].sendTransaction({
-        to: multiSigWallet.address,
-        value: ethers.utils.parseEther("10"),
-      });
-
-      // quorum achived
-      for (let i of [0, 1]) {
-        const tx = await multiSigWallet
-          .connect(signers[i])
-          .confirmTransaction(0);
-        await tx.wait();
-      }
-      await expect(
-        multiSigWallet.connect(signers[0]).executeTransaction(1)
-      ).to.be.revertedWith("tx does not exist");
-    });
-
-    it("should revert if tx is already executed correctly ", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
-
-      await multiSigWallet
-        .connect(signers[0])
-        .submitTransaction(
-          await signers[3].getAddress(),
-          ethers.utils.parseEther("10"),
-          "0x00"
-        );
-      await signers[0].sendTransaction({
-        to: multiSigWallet.address,
-        value: ethers.utils.parseEther("10"),
-      });
-
-      // quorum achived
-      for (let i of [0, 1]) {
-        const tx = await multiSigWallet
-          .connect(signers[i])
-          .confirmTransaction(0);
-        await tx.wait();
-      }
-
-      await multiSigWallet.connect(signers[0]).executeTransaction(0);
-      await expect(
-        multiSigWallet.connect(signers[0]).executeTransaction(0)
-      ).to.be.revertedWith("tx already executed");
-    });
-
     it("tx should execute correctly", async () => {
-      const { multiSigWallet, signers } = await loadFixture(deploy);
+      const to = addr2.address;
+      const value = parseEther("10");
+      const data = "0x00";
 
-      await multiSigWallet
-        .connect(signers[0])
-        .submitTransaction(
-          await signers[3].getAddress(),
-          ethers.utils.parseEther("10"),
-          "0x00"
-        );
-      await signers[0].sendTransaction({
+      await multiSigWallet.connect(addr1).submitTransaction(to, value, data);
+      await addr1.sendTransaction({
         to: multiSigWallet.address,
-        value: ethers.utils.parseEther("10"),
+        value: value,
       });
 
       // quorum achived
-      for (let i of [0, 1]) {
-        const tx = await multiSigWallet
-          .connect(signers[i])
-          .confirmTransaction(0);
-        await tx.wait();
-      }
+      await multiSigWallet.connect(addr1).confirmTransaction(0);
+      await multiSigWallet.connect(addr2).confirmTransaction(0);
 
-      const tx = await multiSigWallet.connect(signers[0]).executeTransaction(0);
+      const tx = await multiSigWallet.connect(addr1).executeTransaction(0);
       const transaction = await multiSigWallet.getTransaction(0);
       expect(transaction.executed).to.be.true;
       await expect(tx)
         .to.emit(multiSigWallet, "ExecuteTransaction")
-        .withArgs(await signers[0].getAddress(), 0);
+        .withArgs(await addr1.getAddress(), 0);
     });
   });
 });
